260ed16a6c9c7f5e32443e1ef7c7b4d188ef7997
aleksey_bulanov <aleksey.bulanov1@gmail.com>
2022-02-23 00:18:17 +0300

    fix title

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -23,7 +23,6 @@ export class ApiService {
   }
   deleteItem(itemId: number) {
     return this.http.delete<void>(`${this.ITEMS_URL}/${itemId}`);
-    
   }
   createItem(payload: IItemModel) {
     return this.http.post<IItemModel>(this.ITEMS_URL, payload);
